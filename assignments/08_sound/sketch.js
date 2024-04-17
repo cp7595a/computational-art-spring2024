9cafae0d594e44cb1cee60476cdfa59eb3bb26a8
Cristina Perez <112970743+cp7595a@users.noreply.github.com>
2024-04-17 14:29:59 -0400

    commit

--- assignments/08_sound/sketch.js
+++ assignments/08_sound/sketch.js
@@ -1,8 +1,14 @@
 let loop;
 let sample;
+let sixteenth = 0;
+let loopInterval = 1;
+let majorArpeggio = [0, 4, 11, 7];
+let root = 60;
+
+// let scale = "major";
 
 function preload() {
-  sample = loadSound("");
+  sample = loadSound("https://cp7595a.github.io/computational-art-spring2024/assignments/08_sound/samples/piano.mp3");
 }
 
 function setup() {
@@ -11,6 +17,8 @@ function setup() {
   background(0, 0, 100);
   
 
+  synth = new p5.PolySynth();
+  loop = new p5.SoundLoop(soundLoop, loopInterval/.5);
 }
 
 function draw() {
@@ -20,12 +28,24 @@ function draw() {
 }
 
 function soundLoop(timeFromNow) {
+  if (sixteenth % 8 === 0) {
+    if (root === 48) {
+      root += 5;
+    } else {
+      root -= 5;
+    }
+  }
   sample.play(timeFromNow)
+
+  let arpIndex = sixteenth % majorArpeggio.length;
+  let note = midiToFreq(root + majorArpeggio[arpIndex]);
+  synth.play(note, 0.8, timeFromNow, 0.8);
+
+  sixteenth++;
 }
 
 function mousePressed() {
   userStartAudio();
 
-  loop = new p5.SoundLoop(soundLoop, loopInterval/4);
-  loop.start(); 
+  // loop.start(); 
 }
